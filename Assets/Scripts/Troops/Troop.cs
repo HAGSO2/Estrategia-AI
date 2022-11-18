4dd565704c113a9d3b22517056e87927c267e61f
Pitarcus <pitarcusmp@gmail.com>
2022-11-18 13:14:06 +0100

    Created animations and prefabs (4 troops)
    Created animation and prefabs for knight, archer, giant and mini-pekka. Discarded having the sprites be inside the scriptable object as it creates confusion and misbehaviour.

--- Assets/Scripts/Troops/Troop.cs
+++ Assets/Scripts/Troops/Troop.cs
@@ -1,22 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 [CreateAssetMenu(fileName = "Troop", menuName = "ScriptableObjects/Troop", order = 1)]
 
 public class Troop : ScriptableObject
 {
     [Header("Parameters")]
-    [SerializeField] float speed = 10f;
-    [SerializeField] float attackRange = 10f;
-    [SerializeField] float health = 10f;
-    [SerializeField] float attacksPerSecond = 2f;
-    [SerializeField] float damage = 10f;
-    [SerializeField] bool onlyAttacksBuildings = false;
-    [SerializeField] int elixirCost = 1;
-
-    [Header("Sprites")]
-    [SerializeField] Sprite[] walkingSprites;
-    [SerializeField] Sprite[] attackSprites;
-    [SerializeField] Sprite[] deathSprites;
+    public float speed = 10f;
+    public float attackRange = 10f;
+    public float health = 10f;
+    public float attacksPerSecond = 2f;
+    public float damage = 10f;
+    public bool onlyAttacksBuildings = false;
+    public int elixirCost = 1;
 }
