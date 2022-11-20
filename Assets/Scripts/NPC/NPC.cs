2d992d6a73982f551807afcf9429ef825df58df7
HAGSO2 <83025616+HAGSO2@users.noreply.github.com>
2022-11-20 14:06:09 +0100

    NPC 2

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -8,18 +8,25 @@ public class NPC : MonoBehaviour
     public Transform t;
     [SerializeField] private Troop Atributes;
     private Rigidbody2D _body;
+    private Animator _controller;
     private float _health;
+    private bool _attackMutex;
+    private bool _eInRange;
 
     private void Start()
     {
         _body = GetComponent<Rigidbody2D>();
+        _controller = GetComponent<Animator>();
         _health = Atributes.health;
         transform.GetChild(0).localScale = new Vector3(Atributes.attackRange / 2,Atributes.attackRange / 2);
+        _attackMutex = true;
+        _eInRange = false;
     }
     
     private void FixedUpdate()
     {
-        MoveTo(t.position);
+        if(_attackMutex)
+            MoveTo(t.position);
     }
 
     private Vector2 ToV2(Vector3 xyz)
@@ -34,13 +41,28 @@ public class NPC : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Destructible"))
+        if (col.CompareTag("Destructible") && _attackMutex)
         {
+            _attackMutex = false;
+            _eInRange = true;
+            Debug.Log("Attack");
+            _controller.SetBool("Attacking",true);
             StartCoroutine(Attack(col.GetComponent<NPC>()));
         }
     }
 
-    public void MoveTo(Vector3 target)
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.CompareTag("Destructible"))
+        {
+            _eInRange = false;
+            Debug.Log("Stop");
+            _controller.SetBool("Attacking",false);
+        }
+    }
+
+    public void MoveTo(Vector3 target,bool seek = true)
     {
         Vector3 desired = target - transform.position;
         if (desired.magnitude > 0.5f)
@@ -53,18 +75,33 @@ public class NPC : MonoBehaviour
     public IEnumerator Attack(NPC other)
     {
         yield return new WaitForSeconds(1 / Atributes.attacksPerSecond);
+        if (!_eInRange)
+        {
+            yield return new WaitForSeconds(1);
+            _attackMutex = true;
+            yield break;
+        }
         try
         {
             other.Hurt(Atributes.damage);
         }
-        catch{yield break;}
-        yield return Attack(other);
+        catch
+        {
+            _attackMutex = true;
+            _controller.SetBool("Attacking",false);
+            yield break;
+        }
+        StartCoroutine(Attack(other));
     }
 
     public void Hurt(float dmg)
     {
+        Debug.Log(gameObject.name + ": " + _health);
         _health -= dmg;
         if (_health <= 0)
-            Destroy(gameObject);
+        {
+            _controller.SetTrigger("Dead");
+            Destroy(this);
+        }
     }
 }
